598ca66ea0c75ed9c2e196327fd8a72e3f1450dd
Lal Gurung <ljgpok@yahoo.com>
2019-04-07 11:24:57 +0300

    bug fix

--- cars/settings.py
+++ cars/settings.py
@@ -142,13 +142,6 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
 
-#Email Config
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_HOST_USER ='' # use your email
-EMAIL_HOST_PASSWORD='' # use your password
-EMAIL_USE_TLS=True
-
 try:
     from local_settings import *
 except ImportError:
