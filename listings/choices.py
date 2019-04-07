704bd623372c76839bba3d0557bfb3785c10a9ff
Lal Gurung <ljgpok@yahoo.com>
2019-04-07 10:02:52 +0300

    first commit

---
+++ listings/choices.py
@@ -0,0 +1,47 @@
+price_choices = {
+  '10000':'€10,000',
+  '20000':'€20,000',
+  '30000':'€30,000',
+  '40000':'€40,000',
+  '50000':'€50,000',
+  '60000':'€60,000',
+  '70000':'€70,000',
+  '80000':'€80,000',
+  '90000':'€90,000',
+  '100000':'€100,000',
+}
+
+state_choices = {
+        'Lapland': 'Lapland',
+        'North Ostrobothnia': 'North Ostrobothnia',
+        'Kainuu': 'Kainuu',
+        'North Karelia': 'North Karelia',
+        'Northern Savonia': 'Northern Savonia',
+        'Southern Savonia': 'Southern Savonia',
+        'South Karelia': 'South Karelia',
+        'Central Finland': 'Central Finland',
+        'South Ostrobothnia': 'South Ostrobothnia',
+        'Ostrobothnia': 'Ostrobothnia',
+        'Central Ostrobothnia': 'Central Ostrobothnia',
+        'Pirkanmaa': 'Pirkanmaa',
+        'Satakunta': 'Satakunta',
+        'Päijänne Tavastia': 'Päijänne Tavastia',
+        'Tavastia Proper': 'Tavastia Proper',
+        'Kymenlaakso': 'Kymenlaakso',
+        'Helsinki-Uusimaa Region': 'Helsinki-Uusimaa Region',
+        'Southwest Finland': 'Southwest Finland',
+        'Åland Islands': 'Åland Islands'
+}
+
+car_brand_choices = {
+      'Toyota': 'Toyota',
+      'BMW': 'BMW',
+      'Volvo': 'Volvo',
+      'Mercedes': 'Mercedes',
+      'Nissan': 'Nissan',
+      'Skoda': 'Skoda',
+      'Lexus': 'Lexus',
+      'Opel': 'Opel',
+      'Tesla': 'Tesla',
+      'Volkswagen': 'Volkswagen'
+}
